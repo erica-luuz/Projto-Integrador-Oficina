a0a9f283705540815ce1042248249f3102e85794
Erica Luz <erica.nicollas_hbn@yahoo.com>
2021-11-21 19:48:57 -0300

    salvamento do bd, msn apos registrar

--- oficina/admin/forms.py
+++ oficina/admin/forms.py
@@ -2,7 +2,7 @@ from wtforms import Form, BooleanField, StringField, PasswordField, validators
 
 
 class RegistrationForm(Form):
-    name = StringField('nome completo :', [validators.Length(min=4, max=25)])
+    name = StringField('Nome completo :', [validators.Length(min=4, max=25)])
     username = StringField('Usuario :', [validators.Length(min=4, max=25)])
     email = StringField('Email :', [validators.Length(min=6, max=35)])
     password = PasswordField('Nova senha', [
--- oficina/admin/rotas.py
+++ oficina/admin/rotas.py
@@ -6,19 +6,26 @@ from .models import User
 import os
 
 
+
+# rota da Home Pagina Administrativa
 @app.route('/')
 def home():
-    return "seja bem vindo ez confecção"
+    return render_template ('admin/index.html', title='Página Administrativa')
 
 
+# rota do cadastro de usuario
 @app.route('/registrar', methods=['GET', 'POST'])
 def registrar():
     form = RegistrationForm(request.form)
     if request.method == 'POST' and form.validate():
+
         hash_password = bcrypt.generate_password_hash(form.password.data)
         user = User(name=form.name.data, username=form.username.data, email=form.email.data,
-        password=hash_password)
+                    password=hash_password)
         db.session.add(user)
-        flash('obrigado por registrar')
+        db.session.commit   #adicionado para salvar no bd
+        flash(f'Obrigado {form.name.data} por registrar', 'success')
+
+
         return redirect(url_for('home'))
-    return render_template('admin/registrar.html', form=form, title="Pagina de Registros")
+    return render_template('admin/registrar.html', form=form, title="Registrar Usuários")
