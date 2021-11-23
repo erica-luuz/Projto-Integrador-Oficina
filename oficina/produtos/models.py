6121b2ae3d5cc30803eb723abc375d3411f71674
Erica Luz <erica.nicollas_hbn@yahoo.com>
2021-11-22 23:01:23 -0300

    modulo fornecedor em produtos concluido com sucesso

--- oficina/produtos/models.py
+++ oficina/produtos/models.py
@@ -3,7 +3,7 @@ from oficina import db
 
 class Fornecedor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(40), nullable=False, unique=False)   # se for palavra unica: unique=True
+    name = db.Column(db.String(40), nullable=False, unique=False)   # se for uma palavra unica: unique=True
     
 class Categoria(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- oficina/produtos/rotas.py
+++ oficina/produtos/rotas.py
@@ -1,8 +1,16 @@
 from flask import redirect, render_template, url_for, flash, request
 
 from oficina import db,  app
-
+from .models import Fornecedor, Categoria
 
 @app.route('/addfornecedor', methods=['GET', 'POST'])
 def addfornecedor():
-    return render_template('/produtos/addfornecedor.html')
+    
+    if request.method =="POST":
+        getfornecedor = request.form.get('fornecedor')
+        fornecedor = Fornecedor(name=getfornecedor)
+        db.session.add(fornecedor)
+        flash(f'O Fornecedor {getfornecedor} foi cadastrado com sucesso', 'success')
+        db.session.commit
+        return redirect(url_for('addfornecedor'))
+    return render_template('/produtos/addfornecedor.html', fornecedor='fornecedor')
