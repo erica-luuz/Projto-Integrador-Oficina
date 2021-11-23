82a54a6f037e12ff611f4751e509347cc17e0198
Erica Luz <erica.nicollas_hbn@yahoo.com>
2021-11-22 23:53:40 -0300

    cadastrar categoria em produtos concluida com sucesso

--- oficina/produtos/rotas.py
+++ oficina/produtos/rotas.py
@@ -2,7 +2,7 @@ from flask import redirect, render_template, url_for, flash, request
 
 from oficina import db,  app
 from .models import Fornecedor, Categoria
-
+# rotas da pag de cadastro de fornecedor.
 @app.route('/addfornecedor', methods=['GET', 'POST'])
 def addfornecedor():
     
@@ -14,3 +14,18 @@ def addfornecedor():
         db.session.commit
         return redirect(url_for('addfornecedor'))
     return render_template('/produtos/addfornecedor.html', fornecedor='fornecedor')
+
+
+# rotas da pag de cadastro de categoria
+
+@app.route('/addcat', methods=['GET', 'POST'])
+def addcat():
+    
+    if request.method =="POST":
+        getfornecedor = request.form.get('categoria')
+        cat = Categoria(name=getfornecedor)
+        db.session.add(cat)
+        flash(f'A Categoria {getfornecedor} foi cadastrada com sucesso', 'success')
+        db.session.commit
+        return redirect(url_for('addcat'))
+    return render_template('/produtos/addfornecedor.html')
